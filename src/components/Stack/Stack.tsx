2803a07c54c8ca986a8b4df1ce0516a050507eff
Khalid Jameer <kjameer0@gmail.com>
2023-05-04 16:18:13 -0400

    feat: removed line height for dynamic resizing of li element

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
-import StyledStack from './StyledStack';
+import { StyledStack } from './StyledStack';
 export default function Stack() {
+  const [stack, setStack] = useState(['go', 'to', 'work', ',hi', 'lol'] as string[]);
   return (
     <StyledStack>
-      <p>Hello</p>
+      <ul>
+        {stack.reverse().map((task, idx) => {
+          return <li key={idx}>{task}</li>;
+        })}
+      </ul>
     </StyledStack>
   );
 }
--- src/components/Stack/StyledStack.ts
+++ src/components/Stack/StyledStack.ts
@@ -1,4 +1,13 @@
 import styled, { css } from 'styled-components';
-export default styled.div`
-  background-color: red;
+export const StyledStack = styled.div`
+  background-color: rgb(150, 150, 150);
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    text-align: center;
+    //outline: auto;
+    font-size: 200px;
+  }
 `;
